ee1cc8ae24d6ead1e4371157009329e147c2c903
Kain Noxfall <kain.noxfall@gmail.com>
2023-03-02 23:14:01 +0100

    Client hooked to Sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 
+import { client } from '@/lib/client';
 import { Product, FooterBanner, HeroBanner } from '@/components/';
 
-export default function Home() {
+export default function Home({ products, bannerData }) {
   return (
     <>
       <Head>
@@ -12,16 +13,28 @@ export default function Home() {
         <link rel="icon" type="image/svg+xml" href="/logo.svg" />
       </Head>
       <main className="app">
-        <HeroBanner />
+        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
         <div className="products-heading">
           <h2>Best Selling Products</h2>
           <p>Speakers of many variations</p>
         </div>
         <div className="products-container">
-          {['Product 1', 'Product 2'].map((product) => product)}
+          {products?.map((product) => product.name)}
         </div>
         <FooterBanner />
       </main>
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+};
